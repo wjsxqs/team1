c908b5ebf09c9cf8cec3e27d73759549145a4013
wjsxqs <xsmlcp@163.com>
2021-02-28 01:55:51 +0800

    Update do_something weight

--- lesson6/substrate-node-template/pallets/template/src/lib.rs
+++ lesson6/substrate-node-template/pallets/template/src/lib.rs
@@ -67,7 +67,11 @@ decl_module! {
 
 		/// An example dispatchable that takes a singles value as a parameter, writes the value to
 		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
-		#[weight = 10_000 + T::DbWeight::get().writes(1)]
+		/// # <weight>
+		/// - Base Weight: 35.87 µs
+		/// - DB Weight: 1 Write
+		/// # </weight>
+		#[weight = T::DbWeight::get().writes(1) + 35_870_000]
 		pub fn do_something(origin, something: u32) -> dispatch::DispatchResult {
 			// Check that the extrinsic was signed and get the signer.
 			// This function will return an error if the extrinsic is not signed.
